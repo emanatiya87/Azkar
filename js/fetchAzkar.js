1680621035d60e225009fec2408a2de3c547b003
Eman <emanatiya87@gmail.com>
2025-02-21 07:45:15 +0200

    desc

--- js/fetchAzkar.js
+++ js/fetchAzkar.js
@@ -23,7 +23,7 @@ function displayAzkar(data) {
     cartona += `
        <br>
          <div class="row">
-            <div class="col-md-8 text-center content">${zekr.content} </div>
+            <div class="col-md-8 text-center content">${zekr.content}<br><span>${zekr.description}</span></div>
             <div class="col-md-4 bigCounter">
               <div
                 class="counter d-flex justify-content-center align-items-center mx-auto"
@@ -32,7 +32,6 @@ function displayAzkar(data) {
                 ${zekr.count}
               </div>
               <div id="reset" onclick="reset(this)" ><i class="fa-solid fa-rotate-right"></i></div>
-  
             </div>
           </div>
            
