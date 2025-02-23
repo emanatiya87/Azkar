974a84bb74fcf382f0683aa4a65e018283d0fb1e
Eman <emanatiya87@gmail.com>
2025-02-23 18:24:44 +0200

    dark mood all pages and remove hesn elmohlm

--- js/darkmood.js
+++ js/darkmood.js
@@ -1,7 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let darkLightIcone = document.getElementById("darkLightIcone");
   let body = document.body;
+
   // Check stored mode and apply it
   if (localStorage.getItem("theme") === "dark") {
     body.classList.add("dark-mode");
   }
+
+  darkLightIcone.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+
+    // Save the current mode in localStorage
+    if (body.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  });
 });
--- js/script.js
+++ js/script.js
@@ -4,24 +4,3 @@ setTimeout(function () {
     loadingPage.style.visibility = "hidden";
   }
 }, 1000);
-
-document.addEventListener("DOMContentLoaded", function () {
-  let darkLightIcone = document.getElementById("darkLightIcone");
-  let body = document.body;
-
-  // Check stored mode and apply it
-  if (localStorage.getItem("theme") === "dark") {
-    body.classList.add("dark-mode");
-  }
-
-  darkLightIcone.addEventListener("click", function () {
-    body.classList.toggle("dark-mode");
-
-    // Save the current mode in localStorage
-    if (body.classList.contains("dark-mode")) {
-      localStorage.setItem("theme", "dark");
-    } else {
-      localStorage.setItem("theme", "light");
-    }
-  });
-});
