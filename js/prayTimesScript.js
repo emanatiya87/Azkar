856df6319ec703631d9a46187b0e9d16b7f285cf
Eman <emanatiya87@gmail.com>
2025-06-04 11:20:08 +0300

    fetch and display times

---
+++ js/prayTimesScript.js
@@ -0,0 +1,68 @@
+let prays = document.getElementById("prays");
+
+// fetch data of times
+fetch("https://api.aladhan.com/v1/timingsByCity?country=EG&city=Al Qāhirah")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.data.timings);
+    displayPrayTimes(data.data.timings);
+  });
+//   display play times
+function displayPrayTimes(time) {
+  prays.innerHTML = `
+     <div class="d-flex justify-content-between align-items-center border rounded-4 px-3 mb-2 shadow">
+        <div >
+          <h3 class="text-info ">الفجر</h3>
+          <h3>${time.Fajr}</h3>
+        </div>
+        <div class=" text-secondary">
+         <i class="fa-solid fa-clock"></i>
+        </div>
+      </div>
+      <div class="d-flex justify-content-between align-items-center border rounded-4 px-3 mb-2 shadow">
+        <div >
+          <h3 class="text-info ">الشروق</h3>
+          <h3>${time.Sunrise}</h3>
+        </div>
+        <div class=" text-secondary">
+        <i class="fa-solid fa-sun"></i>
+        </div>
+      </div>
+      <div class="d-flex justify-content-between align-items-center border rounded-4 px-3 mb-2 shadow">
+        <div >
+          <h3 class="text-info ">الضهر</h3>
+          <h3>${time.Dhuhr}</h3>
+        </div>
+        <div class=" text-secondary">
+         <i class="fa-solid fa-clock"></i>
+        </div>
+      </div>
+      <div class="d-flex justify-content-between align-items-center border rounded-4 px-3 mb-2 shadow">
+        <div >
+          <h3 class="text-info ">العصر</h3>
+          <h3>${time.Asr}</h3>
+        </div>
+        <div class=" text-secondary">
+         <i class="fa-solid fa-clock"></i>
+        </div>
+      </div>
+      <div class="d-flex justify-content-between align-items-center border rounded-4 px-3 mb-2 shadow">
+        <div >
+          <h3 class="text-info ">المغرب</h3>
+          <h3>${time.Maghrib}</h3>
+        </div>
+        <div class=" text-secondary">
+         <i class="fa-solid fa-clock"></i>
+        </div>
+      </div>
+      <div class="d-flex justify-content-between align-items-center border rounded-4 px-3 mb-2 shadow">
+        <div >
+          <h3 class="text-info ">العشاء</h3>
+          <h3>${time.Isha}</h3>
+        </div>
+        <div class=" text-secondary">
+         <i class="fa-solid fa-moon"></i>
+        </div>
+      </div>
+    `;
+}
